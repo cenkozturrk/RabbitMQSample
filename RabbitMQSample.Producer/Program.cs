a91f7b132b9f2e852eef1a8ce04b6b1077d3f5d1
Cenk Öztürk <ccenkozturrk@outlook.com>
2022-08-28 15:08:49 +0300

    Add project files.

---
+++ RabbitMQSample.Producer/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQSample.Producer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri =
+                new Uri("amqp://guest:guest@localhost:15672")
+            };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("demo-queue",
+                durable: true,
+                exclusive: false,
+               autoDelete: false,
+               arguments: null);
+            var message = new { Name = "Producer", Message = "Im Here" };
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+
+            channel.BasicPublish("", "demo-queue", null, body);
+        }
+    }
+}
